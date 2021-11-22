b3a60bb6d2edcc0fa20b4f1e3b95b9f2bdbc5871
jackqj888 <11325176@qq.com>
2021-11-22 17:21:58 +0800

    demo练习1完成

--- vue-demo/src/components/exercise1/index.vue
+++ vue-demo/src/components/exercise1/index.vue
@@ -1,9 +1,35 @@
 <template>
   <div>
-    <LearnTitle title="JS的基本语法点" />
+    <LearnTitle title="JS的基本语法点"/>
 
     <div class="content">
-      <!-- 内容写在这里 -->
+      <!--       内容写在这里-->
+      <div>1:
+        <div class="list">
+          <div>姓名：{{ listData.name }}</div>
+          <div>年龄：{{ listData.age }}</div>
+          <div>学校：{{ listData.school }}</div>
+        </div>
+      </div>
+      <div>2:
+        <div>
+          <div>姓名：{{ newList.name }}</div>
+          <div>年龄：{{ newList.age }}</div>
+          <div>学校：{{ newList.school }}</div>
+        </div>
+      </div>
+      <div>3:
+        <div class="newObj" v-for="(item,index) in newObj" :key="index">
+          <div>第三中学的有：{{ item.name }}</div>
+        </div>
+      </div>
+      <div>4:
+        <div v-for="(item,index) in newObj1" :key="index">
+          <div>姓名：{{ item.name }}</div>
+          <div>年龄：{{ item.age }}</div>
+          <div>学校：{{ item.school }}</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -19,6 +45,7 @@
 // 4. 在数组中找出叫xiaohong的人，然后把他的school改成第三中学，把他的三个字段显示到页面上去，但这里需要对年龄做一个判断，如果年龄大于23就显示“保密”字样
 
 import LearnTitle from "@/components/LearnTitle";
+
 const arrayData = [
   {
     name: "jhon",
@@ -46,12 +73,64 @@ const arrayData = [
     school: "第一中学"
   }
 ];
+
+
 export default {
   name: "exercise1",
   components: {
     LearnTitle
+  },
+
+  data() {
+    return {
+      newData: arrayData,
+      listData: [],
+      newList: {name: '', age: '', school: ''},
+      newObj: [],
+      newObj1: []
+    }
+  },
+  created() {
+    this.listData = this.newData.find(this.newObjs)
+    // console.log(this.listData)
+    // debugger
+
+    let a = this.newData.filter(item => item.name === 'jhon')
+    this.newList.name = a[0].name
+
+    let b = this.newData.filter(item => item.age === 20 && item.name === 'nick')
+    this.newList.age = b[0].age
+
+    let c = this.newData.filter(item => item.school === '第三中学' && item.name === 'xiaoming')
+    this.newList.school = c[0].school
+    // console.log('xxx', this.newList)
+    let d = '第三中学'
+    this.newObj = this.newData.filter(item => item.school === d)
+    let e = 'xiaohong'
+    this.newObj1 = this.newData.filter(item => item.name === e)
+    this.newObj1[0].school = '第三中学'
+    if (this.newObj1[0].age > 23) {
+      this.newObj1[0].age = '保密'
+    }
+    // console.log('ccc', this.newObj1)
+
+
+  },
+  methods: {
+    newObjs(i) {
+      return i.name === 'jhon'
+    },
+
+
   }
+
+
 };
 </script>
 
-<style></style>
+<style>
+.content {
+  width: 700px;
+  height: 500px;
+}
+</style>
